957f0e469606a103f633db801d4a3d317f3860fe
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-12-27 11:16:24 -0300

    feat: rename titles to new one

--- src/modules/contacts/services/GetCsvFieldsService.ts
+++ src/modules/contacts/services/GetCsvFieldsService.ts
@@ -20,19 +20,57 @@ export class GetCsvFieldsService {
 
 		stream.pipe(parseFile)
 
-		return new Promise((res, rej) => {
+		const novos = [{
+			original: 'e-mail',
+			new: 'E-mail'
+		},
+	  {
+			original: 'birth',
+			new: 'Birthday'
+		},
+		{
+			original: 'credit card',
+			new: 'Card type'
+		}
+	]
+
+	return new Promise((res, rej) => {
+			const atualizado: any = []
 			let i = 0
-			let firstLine = ''
+			let firstLine: any[] = []
 			parseFile.on('data', async (line) => {
-					const [name, description] = line
-					console.log(name)
+					const [col1, col2, col3] = line
 					if(i === 0) firstLine = line
-					else parseFile.addListener('end', () => {})
+					else {
+						const [fcol1, fcol2, fcol3] = firstLine
+						const aff = {
+							[fcol1]: col1,
+							[fcol2]: col2,
+							[fcol3]: col3,
+						}
+						atualizado.push(aff)
+						
+					}
+					//console.log(line)
+					//else parseFile.addListener('end', () => {})
 					i++
 			})
 
+			const amem: any = []
+
 			parseFile.on('end', () => {
-				res(firstLine)
+				const newArray = atualizado.map((a, i) => {
+					for(let aa in a) {
+						const thereis = novos.find(n => n.original == aa)
+						if(thereis) {
+							amem.push({
+								[thereis.new]: a[aa]
+							})
+						}
+					}
+				})
+				console.log(amem)
+				res(amem as any)
 			})
 		})
 	}
